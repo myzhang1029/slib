9bdb8a983a4ba0b00f0a07f261e7940d74bf272f
michaelgrand <myzhang1029@hotmail.com>
2017-02-19 09:35:02 +0800

    Update file.c

--- src/file.c
+++ src/file.c
@@ -1,3 +1,23 @@
+/*
+ *  file.c - File editing fuction in the slib
+ *
+ *  Copyright (C) 2001, 2002 Zhang Maiyun
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or (at
+ *  your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
 #include "slib.h"
 
 OPT long  fsize( FILE * stream )
