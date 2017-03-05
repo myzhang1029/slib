7a40ce957fe4903569e74ada38c6555fc0b947e4
myzhang1029 <myzhang1029@163.com>
2017-03-05 13:49:11 +0800

    Edit copyright info

--- src/calc.c
+++ src/calc.c
@@ -5,7 +5,8 @@
  *  Thanks a user on CSDN.NET for the code framework.
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  This file is part of slib.
+ *  slib is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
--- src/slib.h
+++ src/slib.h
@@ -6,7 +6,8 @@
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  This file is part of slib.
+ *  slib is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
