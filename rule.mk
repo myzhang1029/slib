c080e3af3cca82f8f2f30635c2aaf09df24b3d5c
myzhang1029 <myzhang1029@163.com>
2017-05-19 22:49:53 +0800

    Fasten make

--- rule.mk
+++ rule.mk
@@ -9,9 +9,9 @@ win:$(TARGET:end=-win.a)
 unix:$(TARGET:end=-unix.a)
 
 $(TARGET:end=-win.a):$(CFILES)
-	echo $(CFILES) | xargs $(CC) $(CFLAGS-WIN)
+	$(CC) $(CFLAGS-WIN) $^
 	ar rcs ../$(TARGET:end=.a) $(CFILES:.c=.o)
 
 $(TARGET:end=-unix.a):$(CFILES)
-	echo $(CFILES) | xargs $(CC) $(CFLAGS-UNIX)
+	$(CC) $(CFLAGS-UNIX) $^
 	ar rcs ../$(TARGET:end=.a) $(CFILES:.c=.o)
