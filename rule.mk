3b4691f65a93006d9136af58ef972c9e1b66a5c5
myzhang1029 <myzhang1029@163.com>
2017-06-03 19:09:31 +0800

    Shorter make
    Remove some unneed code like
    target:target2
    target2:whatever.o
    
    Add ldflags

--- rule.mk
+++ rule.mk
@@ -2,18 +2,14 @@
 #Licence:LGPL
 #See LICENCE for more details
 
-.PHONY:err win unix $(TARGET:end=-win) $(TARGET:end=-unix)
+include ../config.mk
+
+.PHONY:err win unix
 
 err:;@echo Must be called by the main Makefile
 
-win:$(TARGET:end=-win)
-unix:$(TARGET:end=-unix)
-
-$(TARGET:end=-win):$(CFILES:.c=.o)
-#	$(CC) $(CFLAGS-WIN) $^
-
-$(TARGET:end=-unix):$(CFILES:.c=.o)
-#	$(CC) $(CFLAGS-UNIX) $^
+win:$(CFILES:.c=.o)
+unix:$(CFILES:.c=.o)
 
 %.o:%.c
-	$(CC) $(CFLAGS-$(BUILD)) $^ -o $@
+	$(CC) $(CFLAGS-$(BUILD)) $^ -o $@ $(LDFLAGS)
