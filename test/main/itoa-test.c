9a9d4538a1d529f5e16be1c07dcc532ad44b5122
袁冬梅 <myzhang1029@163.com>
2017-07-15 10:04:45 +0800

    Add test for itoa

---
+++ test/main/itoa-test.c
@@ -0,0 +1,11 @@
+#include <slib.h>
+
+int main(void)
+{
+    char *a="we";
+    int b=12;
+    printf("test-itoa: Original vaule: variable (char*)a with vaule %s\n\tvariable(int)b with vaule %d",a,b);
+    printf("test-itoa: Call itoa(b, a, 10)");
+    printf("test-itoa: Now b = %d, a = %s", b, d);
+    return 0;
+}
