a105bef8c4ee3b0bbb9348a17a3957cf5fc8a9f3
袁冬梅 <myzhang1029@163.com>
2017-07-22 22:59:42 +0800

    Add build xmake

---
+++ xmake.lua
@@ -0,0 +1,42 @@
+-- debug mode
+if is_mode("debug") then
+	
+	-- enable the debug symbols
+	set_symbols("debug")
+
+	-- disable optimization
+	set_optimize("none")
+end
+
+-- release mode
+if is_mode("release") then
+
+	-- set the symbols visibility: hidden
+	set_symbols("hidden")
+
+	-- enable fastest optimization
+	set_optimize("fastest")
+
+	-- strip all symbols
+	set_strip("all")
+end
+
+-- add target
+target("sbl")
+
+	-- set kind
+	set_kind("shared")
+
+	-- add cflags
+	add_cflags("-Iinclude")
+
+	-- add install
+	add_headers("include/slib.h")
+
+	-- add files
+	add_files("file/*.c")
+	add_files("main/*.c")
+	add_files("math/*.c")
+	add_files("string/*.c")
+	add_files("stack/*.c")
+
