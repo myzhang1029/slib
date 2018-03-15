a9b35c7a71ecef85b53285bc4b86533d619aac82
myzhang1029 <myzhang1029@163.com>
2018-03-16 06:51:26 +0800

    format

--- admin.c
+++ admin.c
@@ -1,4 +1,3 @@
-
 /*
  *  admin.c – The admin project
  *
@@ -23,94 +22,92 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 /* 包含头文件 */
-#include "slib.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-ccp ver = "3.2.2";                           /*版本号*/
+#include "slib.h"
+ccp ver = "3.2.2"; /*版本号*/
 
+void usage(void); /*使用方法*/
 
-void usage( void );/*使用方法*/ 
-
-
-int main( int argc, const char *argv[] )	/* 主函数 */
+int main(int argc, const char *argv[]) /* 主函数 */
 {
 	union main_variable
 	{
 		unsigned int content;
 		unsigned int shutdownsys;
 	} m_v;
-	//char *soptions=":hr:g:l:p:d:c:";
-	if (argc>1)
+	// char *soptions=":hr:g:l:p:d:c:";
+	if (argc > 1)
 	{
-		if(strcmp(argv[1],"-u")==0);
-		else if(strcmp(argv[1],"-h")==0)
+		if (strcmp(argv[1], "-u") == 0)
+			;
+		else if (strcmp(argv[1], "-h") == 0)
 		{
 			usage();
 			return 0;
 		}
-		else if(strcmp(argv[1],"-r")==0)
+		else if (strcmp(argv[1], "-r") == 0)
 		{
-			if(argc<4)
+			if (argc < 4)
 				usage();
 			else
 			{
-				if(isrp(atol(argv[2]),
-				        atol(argv[3]))==STRUE)
+				if (isrp(atol(argv[2]), atol(argv[3])) == STRUE)
 					printf("互质！");
 				else
 					printf("不互质！");
 			}
 			return 0;
 		}
-		else if(strcmp(argv[1],"-g")==0)
+		else if (strcmp(argv[1], "-g") == 0)
 		{
-			if(argc<4)
+			if (argc < 4)
 				usage();
 			else
-				printf("%lu",gcf(atol(argv[2]),atol(argv[3])));
+				printf("%lu", gcf(atol(argv[2]), atol(argv[3])));
 			return 0;
 		}
-		else if(strcmp(argv[1],"-l")==0)
+		else if (strcmp(argv[1], "-l") == 0)
 		{
-			if(argc<4)
+			if (argc < 4)
 				usage();
 			else
-				printf("%lu",lcm(atol(argv[2]),atol(argv[3])));
+				printf("%lu", lcm(atol(argv[2]), atol(argv[3])));
 			return 0;
 		}
-		else if(strcmp(argv[1],"-p")==0)
+		else if (strcmp(argv[1], "-p") == 0)
 		{
-			if(argc<4)
+			if (argc < 4)
 				usage();
 			else
-				prtpn(stdout,atol(argv[2]),atol(argv[3]));
+				prtpn(stdout, atol(argv[2]), atol(argv[3]));
 			return 0;
 		}
-		else if(strcmp(argv[1],"-d")==0)
+		else if (strcmp(argv[1], "-d") == 0)
 		{
-			if(argc<3)
+			if (argc < 3)
 				usage();
 			else
 			{
-				if(ispn(atol(argv[2]))==STRUE)
+				if (ispn(atol(argv[2])) == STRUE)
 					printf("是质数！");
 				else
 					printf("不是质数！");
 			}
 			return 0;
 		}
-		else if(strcmp(argv[1],"-c")==0)
+		else if (strcmp(argv[1], "-c") == 0)
 		{
-			if(argc<3)
+			if (argc < 3)
 				usage();
 			else
 			{
 				double result;
 				puts(argv[2]);
-				result=calculate(argv[2]);
-				printf("%s",argv[2]);
-				printf("%lf",result);
+				result = calculate(argv[2]);
+				printf("%s", argv[2]);
+				printf("%lf", result);
 			}
 			return 0;
 		}
@@ -120,104 +117,103 @@ int main( int argc, const char *argv[] )	/* 主函数 */
 			return 0;
 		}
 	}
-	for(;;)
+	for (;;)
 	{
-		printf( "\n1=查看系统版本\n2=产生随机数\n3=计算器\n4=自动关机\n5=输出指定范围内的质数"
-		"\n6=判断一个数是否质数\n7=判断两数是否互质\n请选择：");	/*程序主菜单*/
-		scanf( "%d", &m_v.content );
-		switch ( m_v.content )
+		printf("\n1=查看系统版本\n2=产生随机数\n3=计算器\n4=自动关机\n5=输出指定范围内的质数"
+		       "\n6=判断一个数是否质数\n7=判断两数是否互质\n请选择："); /*程序主菜单*/
+		scanf("%d", &m_v.content);
+		switch (m_v.content)
 		{
-		case 1:	/*显示软件版本*/
-		{
-			printf( "sbl admin %s\n", ver );
-			printf( "于%s，%s发布\n", __DATE__, __TIME__ );
-			break;
-		}
+			case 1: /*显示软件版本*/
+			{
+				printf("sbl admin %s\n", ver);
+				printf("于%s，%s发布\n", __DATE__, __TIME__);
+				break;
+			}
 
-		case 2:
-		{
-			unsigned int radmax = 0,radmin = 0;	/*随机数变量*/
-			printf( "输入最小值：" );
-			scanf( "%d",&radmin );
-			printf( "输入最大值：" );
-			scanf( "%d",&radmax );
-			printf( "您的随机数是：%ld\n\n", randomnum(clock(),radmax,radmin) );
-			break;
-		}
+			case 2:
+			{
+				unsigned int radmax = 0, radmin = 0; /*随机数变量*/
+				printf("输入最小值：");
+				scanf("%d", &radmin);
+				printf("输入最大值：");
+				scanf("%d", &radmax);
+				printf("您的随机数是：%ld\n\n", randomnum(clock(), radmax, radmin));
+				break;
+			}
 
-		case 3: /*计算器*/
-		{
-			char	infix[BUFFERSIZE];
-			fflush( stdin );
-			printf( "输入算式：" );
-			fflush( stdin );
-			fgets( infix, BUFFERSIZE, stdin );
-			printf( "%lf\n", calculate( infix ) );	/*使用libsbl中的函数进行计算*/
-			break;
-		}
+			case 3: /*计算器*/
+			{
+				char infix[BUFFERSIZE];
+				fflush(stdin);
+				printf("输入算式：");
+				fflush(stdin);
+				fgets(infix, BUFFERSIZE, stdin);
+				printf("%lf\n", calculate(infix)); /*使用libsbl中的函数进行计算*/
+				break;
+			}
 
-		case 4:	/*关机器*/
-		{
-			#if PLAT
-			system( "shutdown /t 20" );
-			#else
-			system( "shutdown +0.3");
-			#endif
-			printf( "输入1来阻止关闭系统" );
-			if ( scanf( "%d", &m_v.shutdownsys ) == 1 )
-			#if PLAT
-				system( "shutdown /a" );
-			#else
-				system("shutdown -c");
-			#endif
-			break;
-		}
+			case 4: /*关机器*/
+			{
+#if PLAT
+				system("shutdown /t 20");
+#else
+				system("shutdown +0.3");
+#endif
+				printf("输入1来阻止关闭系统");
+				if (scanf("%d", &m_v.shutdownsys) == 1)
+#if PLAT
+					system("shutdown /a");
+#else
+					system("shutdown -c");
+#endif
+				break;
+			}
 
-		case 5:	/*输出指定范围内的指数 */
-		{
-			int pn1,pn2;
-			printf("输入最小值：");
-			scanf("%d",&pn1);
-			printf("\n输入最大值：");
-			scanf("%d",&pn2);
-			prtpn(stdout,pn1,pn2);
-			break;
-		}
-		case 6:	/*测试一个数是否质数*/
-		{
-			int pn;
-			printf("输入待测试数:");
-			scanf("%d",&pn);
-			if (ispn(pn)==STRUE)
-				printf("\n是质数！\n");
-			else
-				printf("\n不是质数！\n");
-			break;
-		}
-		case 7:	/*测试两个数是否互质*/
-		{
-			int num1,num2;
-			printf("输入数一:");
-			scanf("%d",&num1);
-			printf("\n输入数二:");
-			scanf("%d",&num2);
-			if(isrp(num1,num2)==STRUE)
-				printf("\n互质！\n");
-			else
-				printf("不互质！\n");
-			break;
-		}
-		case 'q':
-			return 0;
-		default:
-			printf("%d: Unknown option\n", m_v.content);
-		}/*switch(m_v.content)*/
+			case 5: /*输出指定范围内的指数 */
+			{
+				int pn1, pn2;
+				printf("输入最小值：");
+				scanf("%d", &pn1);
+				printf("\n输入最大值：");
+				scanf("%d", &pn2);
+				prtpn(stdout, pn1, pn2);
+				break;
+			}
+			case 6: /*测试一个数是否质数*/
+			{
+				int pn;
+				printf("输入待测试数:");
+				scanf("%d", &pn);
+				if (ispn(pn) == STRUE)
+					printf("\n是质数！\n");
+				else
+					printf("\n不是质数！\n");
+				break;
+			}
+			case 7: /*测试两个数是否互质*/
+			{
+				int num1, num2;
+				printf("输入数一:");
+				scanf("%d", &num1);
+				printf("\n输入数二:");
+				scanf("%d", &num2);
+				if (isrp(num1, num2) == STRUE)
+					printf("\n互质！\n");
+				else
+					printf("不互质！\n");
+				break;
+			}
+			case 'q':
+				return 0;
+			default:
+				printf("%d: Unknown option\n", m_v.content);
+		} /*switch(m_v.content)*/
 	}
 	return 0;
-}/*main*/
+} /*main*/
 
-
-void usage( void )
+void usage(void)
 {
 	printf("Usage:admin [-u] [-h] [-r <n1> <n2>] [-g <n1> <n2>] [-l <n1> <n2>]\n"
 	       "\t[-p <n1> <n2>] [-d <num>] [-c <exp>]\n"
