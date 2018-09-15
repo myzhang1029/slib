3fe37d86eaefbe40a21bebd3e1d2f85fb57104a5
myzhang1029 <myzhang1029@163.com>
2018-09-15 15:57:45 +0800

    Fully migrate to CMake and add a autoconf wrapper

---
+++ cmake/distclean.cmake
@@ -0,0 +1,26 @@
+set(cmake_generated
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeCache.txt
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/cmake_install.cmake
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/install_manifest.txt
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/Makefile
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeFiles
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/sbl.pc
+)
+foreach(file ${cmake_generated})
+    if (EXISTS ${file})
+        message(STATUS "Removing file " ${file})
+        file(REMOVE_RECURSE ${file})
+    endif()
+endforeach(file)
+
+# if running dir is not . and is empty, delete it
+if (RUNNING_DIR AND NOT RUNNING_DIR EQUAL "."
+        AND NOT RUNNING_DIR EQUAL "./"
+        AND NOT RUNNING_DIR EQUAL "")
+    string(CONCAT PATT ${RUNNING_DIR} "/*")
+    file(GLOB RES ${PATT})
+    list(LENGTH RES RES_LEN)
+    if(RES_LEN EQUAL 0)
+        file(REMOVE_RECURSE ${RUNNING_DIR})
+    endif()
+endif()
