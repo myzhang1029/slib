1e03e6f3d8db9af9566c6a8c7f17c9c4d1a213b4
myzhang1029 <myzhang1029@163.com>
2018-10-13 21:06:05 +0800

    Add string assertions

--- test/itoa-test.c
+++ test/itoa-test.c
@@ -7,15 +7,15 @@ int main(void)
     unsigned long ul = 12;
     char *result = malloc(4);
     itoaS(i, result, 10);
-    asrt_equ(strcmp(result, "12"), 0, "itoaS");
+    asrt_str_equ(result, "12", "itoaS");
     ltoaS(l, result, 10);
-    asrt_equ(strcmp(result, "12"), 0, "ltoaS");
+    asrt_str_equ(result, "12", "ltoaS");
     ltoaS(-l, result, 10);
-    asrt_equ(strcmp(result, "-12"), 0, "ltoaS");
+    asrt_str_equ(result, "-12", "ltoaS");
     ultoaS(ul, result, 16);
-    asrt_equ(strcmp(result, "c"), 0, "ultoaS");
+    asrt_str_equ(result, "c", "ultoaS");
     ultoaS(ul, result, 12);
-    asrt_equ(strcmp(result, "10"), 0, "ultoaS");
+    asrt_str_equ(result, "10", "ultoaS");
     free(result);
     return 0;
 }
--- test/tests.h
+++ test/tests.h
@@ -1,6 +1,8 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #ifdef QUIET
 #define asrt_or_fail(e, s) ((e) ? (0) : (abort(), 0))
 #else
@@ -11,13 +13,19 @@
 #ifndef DEBUG
 #define asrt_equ(a, b, s) asrt_or_fail((a) == (b), s)
 #define asrt_neq(a, b, s) asrt_or_fail((a) != (b), s)
+#define asrt_str_equ(a, b, s) asrt_or_fail(strcmp((a), (b)) == 0, s)
+#define asrt_str_neq(a, b, s) asrt_or_fail(strcmp((a), (b)) != 0, s)
 #else
 #define asrt_equ(a, b, s)                                                      \
-    (printf("The result should be %d, and it's actually %d\n", (b), (a)),      \
+    (printf("The result should be \"%s\", and it's actually \"%s\"\n", (b), (a)),      \
      asrt_or_fail((a) == (b), s))
 #define asrt_neq(a, b, s)                                                      \
-    (printf("The result should not be %d, and it's actually %d\n", (b), (a)),  \
+    (printf("The result should not be \"%s\", and it's actually \"%s\"\n", (b), (a)),  \
      asrt_or_fail((a) != (b), s))
+#define asrt_str_equ(a, b, s)                                                      \
+    (printf("The result should be \"%s\", and it's actually \"%s\"\n", (b), (a)),      \
+     asrt_or_fail(strcmp((a), (b)) == 0, s))
+#define asrt_str_neq(a, b, s)                                                      \
+    (printf("The result should not be \"%s\", and it's actually \"%s\"\n", (b), (a)),  \
+     asrt_or_fail(strcmp((a), (b)) != 0, s))
 #endif
-#define quiet_asrt_equ(a, b, s) assert((a) == (b))
-#define quiet_asrt_neq(a, b, s) assert((a) != (b))
