27ba89c094b7ffb4d9a652d84a36cc1fed5cd332
myzhang1029 <myzhang1029@163.com>
2018-10-13 19:41:44 +0800

    Finish test-itoa

--- test/itoa-test.c
+++ test/itoa-test.c
@@ -5,12 +5,15 @@ int main(void)
     int i = 12;
     long l = 12;
     unsigned long ul = 12;
-    char *result = "init";
+    char *result = malloc(4);
     itoaS(i, result, 10);
     asrt_equ(result, "12", "itoaS");
     ltoaS(l, result, 10);
     asrt_equ(result, "12", "ltoaS");
-    ultoaS(ul, result, 10);
-    asrt_equ(result, "12", "ultoaS");
+    ultoaS(ul, result, 16);
+    asrt_equ(result, "c", "ultoaS");
+    ultoaS(-ul, result, 10);
+    asrt_equ(result, "-12", "ultoaS");
+    free(result);
     return 0;
 }
