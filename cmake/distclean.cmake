9f4d234c2ffbcbda8540c407c55edee4c6d18b41
myzhang1029 <myzhang1029@163.com>
2018-11-02 23:04:38 +0800

    distclean for CPack

--- cmake/distclean.cmake
+++ cmake/distclean.cmake
@@ -1,25 +1,32 @@
 set(cmake_generated
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeCache.txt
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/cmake_install.cmake
-    ${CMAKE_BINARY_DIR}/uninstall.cmake
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/install_manifest.txt
+    ${CMAKE_BINARY_DIR}/uninstall.cmake
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/Makefile
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CTestTestfile.cmake
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeFiles
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/rules.ninja
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/build.ninja
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/.ninja_deps
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/.ninja_log
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeFiles
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CTestTestfile.cmake
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CPackConfig.cmake
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CPackSourceConfig.cmake
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/_CPack_Packages
+    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/libsbl-?.?.?-*
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/Testing
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/sbl.pc
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/testdrv.h
     ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/config.h
 )
 foreach(file ${cmake_generated})
-    if (EXISTS ${file})
-        message(STATUS "Removing file " ${file})
-        file(REMOVE_RECURSE ${file})
-    endif()
+    file(GLOB file ${file})
+    foreach(f ${file})
+        if (EXISTS ${f})
+            message(STATUS "Removing file " ${f})
+            file(REMOVE_RECURSE ${f})
+        endif()
+    endforeach(f)
 endforeach(file)
 
 # if running dir is not . and is empty, delete it
