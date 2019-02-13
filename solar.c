e3b728d272102897692322f865f6328e7a7bc55b
myzhang1029 <myzhang1029@163.com>
2019-02-13 11:46:28 +0800

    All of solar?

--- solar.c
+++ solar.c
@@ -3,9 +3,12 @@
  * 1. "Sunrise equation": Wikipedia
  * 2. "Julian Day": Wikipedia
  * Those are available under CC-BY-SA.
- * Other formulas in this file are my own thoughts */
-/*
- * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA. *
+ * Other formulas in this file are my own thoughts.
+ *
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+ * Unported License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA. */
 #include <math.h>
 #include <time.h>
 
@@ -19,8 +22,8 @@
 #define datan(arg) slib_rad2deg(atan(arg))
 /* leap year: every four years before 1582 or exclude non-four-hundredth if it's
  * a centennial year */
-#define isleap(year)                                                           \
-    (year % 400 == 0 || (year % 4 == 0 && (year < 1582 || year % 100 != 0)))
+/* leap year here: proleptic */
+#define isleap(year) (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
 
 /* radian and degree conversion, stable API */
 double slib_rad2deg(double radians) { return radians * 180 / S_PI; }
@@ -90,18 +93,6 @@ double slib_sun_decl_by_date(int d)
                   dcos(0.98565 * (d + 10) + 1.914 * dsin(0.98565 * (d - 2))));
 }
 
-/* convert tm structure to Julian Date [2] */
-double slib_julian_date(struct tm tm)
-{
-    int y = tm.tm_year + 1900, m = tm.tm_mon + 1, d = tm.tm_mday;
-    double jdn = (1461 * (y + 4800 + (m - 14) / 12)) / 4 +
-                 (367 * (m - 2 - 12 * ((m - 14) / 12))) / 12 -
-                 (3 * ((y + 4900 + (m - 14) / 12) / 100)) / 4 + d - 32075;
-    /* note the result of slib_hms2h might be negative */
-    return jdn + (tm.tm_hour + 1.0 - 12.0 /* midnight2noon */) / 24.0 +
-           tm.tm_min / 1440.0 + tm.tm_sec / 86400.0;
-}
-
 /* get the time difference(in hours) between UTC and true local time
    (i.e. the sun is exactly south, north or overhead at 12:00)
    longitude in degrees */
@@ -123,17 +114,108 @@ double slib_true_time_diff(double longitude)
     }
     return longitude / 15.0;
 }
+
 double slib_local_time(double longitude, double hours, double timezone)
 {
     return hours - timezone + slib_true_time_diff(longitude);
 }
-/* convert hour angle to hour difference */
+
+/* convert hour angle to hour difference, stable API */
 double slib_hour_angle_to_hour_diff(double hour_angle)
 {
     return hour_angle / 15.0;
 }
 
-/* calculate current julian day from slib_julian_date [1] */
+/* convert tm structure to Julian Date [2], stable API */
+double slib_tm2jd(struct tm tm)
+{
+    int y = tm.tm_year + 1900, m = tm.tm_mon + 1, d = tm.tm_mday;
+    double jdn = (1461 * (y + 4800 + (m - 14) / 12)) / 4 +
+                 (367 * (m - 2 - 12 * ((m - 14) / 12))) / 12 -
+                 (3 * ((y + 4900 + (m - 14) / 12) / 100)) / 4 + d - 32075;
+    /* note the result of slib_hms2h might be negative */
+    return jdn + (tm.tm_hour - 12.0 /* midnight2noon */) / 24.0 +
+           tm.tm_min / 1440.0 + tm.tm_sec / 86400.0;
+}
+
+/* convert Julian Date back to tm structure */
+void slib_jd2tm(double jd, struct tm *tm)
+{
+    int jdn = (int)jd;
+    double frac = jd - jdn;
+    int day = 1, month = 1, year = -4713;
+    double hour, min, sec;
+    /* TODO */
+    for (;; ++year)
+    {
+        if (isleap(year))
+        {
+            if (jdn < 366) /* current year found */
+                break;
+            jdn -= 366;
+        }
+        else
+        {
+            if (jdn < 365) /* current year found */
+                break;
+            jdn -= 365;
+        }
+    }
+    for (;; ++month)
+    {
+        int n = 28;
+        if (isleap(year))
+            n = 29;
+        if (month == 1 || month == 3 || month == 5 || month == 7 ||
+            month == 8 || month == 10 || month == 12)
+        {
+            if (jdn < 31) /* current month found */
+                break;
+            jdn -= 31;
+        }
+        else if (month == 4 || month == 6 || month == 9 || month == 11)
+        {
+            if (jdn < 30) /* current month found */
+                break;
+            jdn -= 30;
+        }
+        else /* February */
+        {
+            if (jdn < n)
+                break;
+            jdn -= n;
+        }
+    }
+    day = jdn;
+    if (frac >= 0.5) /* the next day */
+    {
+        ++day;
+        frac -= 0.5;
+        slib_h2hms(frac / 3600.0, &hour, &min, &sec);
+    }
+    else /* today afternoon/evening */
+    {
+        frac += 0.5;
+        slib_h2hms(frac / 3600.0, &hour, &min, &sec);
+    }
+
+    printf("%4d/%02d/%02d,%02d:%02d:%02d\n", year, month, day, (int)hour,
+           (int)min, (int)sec);
+    tm->tm_sec = sec;
+    tm->tm_min = min;
+    tm->tm_mday = day;
+    tm->tm_mon = month - 1;
+    tm->tm_year = year - 1900;
+    /* must I fill out the whole structure? */
+    if (frac < 0.5)
+        tm->tm_wday = (jdn + 1) % 7;
+    else
+        tm->tm_wday = jdn % 7 + 1;
+    tm->tm_yday = slib_d2dn(year, month, day);
+    tm->tm_isdst = -1;
+}
+
+/* calculate current Julian Day from Julian Date [1] */
 double slib_julian_day(double julian_date)
 {
     return julian_date - 2451545.0 + 0.0008;
@@ -143,6 +225,42 @@ double slib_julian_day(double julian_date)
 double slib_mean_solar_noon(double julian_day, double longitude)
 {
     return julian_day - longitude / 360;
+}
+
+/* solar mean anomaly [1] */
+double slib_solar_mean_anomaly(double mean_solar_noon)
+{
+    return fmod(357.5291 + 0.98560028 * mean_solar_noon, 360);
+}
+
+/* equation of the center [1] */
+double slib_equation_of_the_center_value(double solar_mean_anomaly)
+{
+    return 1.9148 * dsin(solar_mean_anomaly) +
+           0.0200 * dsin(2 * solar_mean_anomaly) +
+           0.0003 * dsin(3 * solar_mean_anomaly);
+}
+
+/* ecliptic longitude [1]
+ * eoc: equation of the center value */
+double slib_ecliptic_longitude(double solar_mean_anomaly, double eoc_value)
+{
+    return fmod(solar_mean_anomaly + eoc_value + 180 + 102.9372, 360);
+}
+
+/* solar transit [1]
+ * returns a julian date */
+double slib_solar_transit(double mean_solar_noon, double solar_mean_anomaly,
+                          double ecliptic_longitude)
+{
+    return 2451545.0 + mean_solar_noon + 0.0053 * dsin(solar_mean_anomaly) -
+           0.0069 * dsin(2 * ecliptic_longitude);
+}
+
+/* declination of the sun [1] */
+double slib_sun_decl(double ecliptic_longitude)
+{
+    return dasin(dsin(ecliptic_longitude) * dsin(23.44));
 }
 
 /* the sunrise equation [1]
@@ -166,19 +284,37 @@ double slib_corr_sunrise_hour_angle(double latitude, double sun_declination,
                  (dcos(latitude) * dcos(sun_declination)));
 }
 
-int main()
+/* single function version */
+double slib_sf_csha(double latitude, double longitude, double elevation,
+                    double *solartrans, struct tm tm)
 {
-    double h, m, s;
+    double jd = slib_tm2jd(tm);
+    double jdy = slib_julian_day(jd);
+    double msn = slib_mean_solar_noon(jdy, longitude);
+    double sma = slib_solar_mean_anomaly(msn);
+    double eoc = slib_equation_of_the_center_value(sma);
+    double el = slib_ecliptic_longitude(sma, eoc);
+    double sundec = slib_sun_decl(el);
+    *solartrans = slib_solar_transit(msn, sma, el);
+    return slib_corr_sunrise_hour_angle(latitude, sundec, elevation);
+}
+
+#include <stdio.h>
+int test()
+{
     time_t rawtm;
     struct tm *tm;
+    double sunset, sha, st, lat = 25.0, lon = 102.7, elev = 1904, jd;
     time(&rawtm);
     tm = localtime(&rawtm);
-    slib_h2hms(slib_local_time(102.690873, 12, 8), &h, &m, &s);
-    printf("%f\n", slib_julian_date(*tm));
-    printf("%lf:%lf:%lf\n", h, m, s);
-    printf("%lf\n", slib_sun_decl_by_date(slib_d2dn(2018, 8, 30)));
-    printf("%lf\n", slib_hour_angle_to_hour_diff(slib_corr_sunrise_hour_angle(
-                        25.057672,
-                        slib_sun_decl_by_date(slib_d2dn(2018, 8, 30)), 1904)));
+    jd = slib_tm2jd(*tm);
+    slib_jd2tm(jd, tm);
+    sha = slib_sf_csha(lat, lon, elev, &st, *tm);
+    sunset = st + sha / 360;
+    slib_jd2tm(sunset, tm);
+    printf("%4d/%02d/%02d,%02d:%02d:%02d\n%lf\n", tm->tm_year + 1900,
+           tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec,
+           sunset);
+
     return 0;
 }
