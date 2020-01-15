56a282e7bf91a3b38d9cae50d077df5b5fdf40fd
Zhang Maiyun <myzhang1029@163.com>
2020-01-15 22:47:55 +0800

    Remove debug code

--- test/gcf-test.c
+++ test/gcf-test.c
@@ -5,7 +5,6 @@ int gcf_main(void)
     asrt_equ(slib_gcf(1, 2), 1, "gcf-1,2");
     asrt_equ(slib_gcf(2, 4), 2, "gcf-2,4");
     asrt_equ(slib_gcf(17, 23), 1, "gcf-17,23");
-    printf("%llu",slib_gcf(18, 23), 1, "gcf-17,23");
     asrt_equ(slib_gcf(3, 9), 3, "gcf-3,9");
     return 0;
 }
