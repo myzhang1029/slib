fb200007bac05ecedb2b7d7a20f1b3dd2cf933ac
myzhang1029 <myzhang1029@163.com>
2020-01-22 11:08:31 +0800

    Improve mret-test

--- test/mret-test.c
+++ test/mret-test.c
@@ -5,9 +5,13 @@ mtret *testfunc(const char *arg1, const char *arg2) { mtreturn(2, arg1, arg2); }
 
 int mret_main(void)
 {
+    char *retstr;
     mtret *ret = testfunc("123", "456");
-    asrt_str_equ((char *)getret(ret), "123", "mret-stage1");
-    asrt_str_equ((char *)getret(ret), "456", "mret-stage2");
-    asrt_equ(getret(ret), NULL, "mret-stage3");
+    retstr = (char *)getret(ret);
+    asrt_str_equ(retstr, "123", "mret-stage1");
+    retstr = (char *)getret(ret);
+    asrt_str_equ(retstr, "456", "mret-stage2");
+    retstr = (char *)getret(ret);
+    asrt_equ(retstr, NULL, "mret-stage3");
     return 0;
 }
