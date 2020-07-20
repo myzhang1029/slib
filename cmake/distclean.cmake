533e7de601cef26bfca047b8d392f98a37a8612e
Zhang Maiyun <myzhang1029@hotmail.com>
2020-07-20 19:46:59 +0800

    Use new distclean

--- cmake/distclean.cmake
+++ cmake/distclean.cmake
@@ -1,42 +1,51 @@
-set(cmake_generated
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeCache.txt
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/cmake_install.cmake
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/install_manifest.txt
-    ${CMAKE_BINARY_DIR}/uninstall.cmake
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/Makefile
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeFiles
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/rules.ninja
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/build.ninja
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/.ninja_deps
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/.ninja_log
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CTestTestfile.cmake
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CPackConfig.cmake
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CPackSourceConfig.cmake
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/_CPack_Packages
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/libsbl-?.?.?-*
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/Testing
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/sbl.pc
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/testdrv.h
-    ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/config.h
-)
-foreach(file ${cmake_generated})
-    file(GLOB file ${file})
-    foreach(f ${file})
-        if (EXISTS ${f})
-            message(STATUS "Removing file " ${f})
-            file(REMOVE_RECURSE ${f})
-        endif()
-    endforeach(f)
-endforeach(file)
+# Distclean.cmake: Clean all CMake-generated files
+# Public domain
+# Author Zhang Maiyun <myzhang1029@hotmail.com>
 
-# if running dir is not . and is empty, delete it
-if (RUNNING_DIR AND NOT RUNNING_DIR EQUAL "."
+foreach(RUNNING_DIR ${subdirs})
+    list(APPEND cmake_generated
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/libsbl-?.?.?-*
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/sbl.pc
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/testdrv.h
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/config.h
+
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeCache.txt
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/cmake_install.cmake
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/install_manifest.txt
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/compile_commands.json
+        ${CMAKE_BINARY_DIR}/uninstall.cmake
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/Makefile
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CMakeFiles
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/rules.ninja
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/build.ninja
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/.ninja_deps
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/.ninja_log
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/Testing
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CTestTestfile.cmake
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CPackConfig.cmake
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/CPackSourceConfig.cmake
+        ${CMAKE_BINARY_DIR}/${RUNNING_DIR}/_CPack_Packages
+    )
+
+    foreach(file ${cmake_generated})
+        file(GLOB file ${file})
+        foreach(f ${file})
+            if (EXISTS ${f})
+                message(STATUS "Removing file " ${f})
+                file(REMOVE_RECURSE ${f})
+            endif()
+        endforeach(f)
+    endforeach(file)
+
+    # if running dir is not . and is empty, delete it
+    if (RUNNING_DIR AND NOT RUNNING_DIR EQUAL "."
         AND NOT RUNNING_DIR EQUAL "./"
         AND NOT RUNNING_DIR EQUAL "")
-    string(CONCAT PATT ${RUNNING_DIR} "/*")
-    file(GLOB RES ${PATT})
-    list(LENGTH RES RES_LEN)
-    if(RES_LEN EQUAL 0)
-        file(REMOVE_RECURSE ${RUNNING_DIR})
+        string(CONCAT PATT ${RUNNING_DIR} "/*")
+        file(GLOB RES ${PATT})
+        list(LENGTH RES RES_LEN)
+        if(RES_LEN EQUAL 0)
+            file(REMOVE_RECURSE ${RUNNING_DIR})
+        endif()
     endif()
-endif()
+endforeach(RUNNING_DIR)
