ed8a7cb171d35a700a19491794aec38456d3f295
Zhang Maiyun <myzhang1029@hotmail.com>
2020-11-16 15:22:19 +0800

    Fix non-posix script

--- cmake/version.sh
+++ cmake/version.sh
@@ -4,5 +4,4 @@ srcdir="$(dirname "$0")/.."
 ver_major="$(grep SBLLIB_VERSION "${srcdir}/include/slib/general.h" | cut -f3 -d\ )"
 ver_minor="$(grep SBLLIB_MINOR "${srcdir}/include/slib/general.h" | cut -f3 -d\ )"
 ver_pl="$(grep SBLLIB_PATCHLEVEL "${srcdir}/include/slib/general.h" | cut -f3 -d\ )"
-version="${ver_major}.${ver_minor}.${ver_pl}"
-echo -n "$version"
+echo "${ver_major}.${ver_minor}.${ver_pl}" | tr -d '\r\n'
